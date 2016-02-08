9baffd4aa9ed57947edde0dc6963ea4e77d5cc9d
teturo <teturou.02@gmail.com>
2016-02-08 20:38:16 +0900

    rename & update rim.h

--- rim.h
+++ rim.h
@@ -15,3 +15,15 @@ struct rim_data
 	double erd;
 	double weight;
 };
+
+struct rim_data cxp22 = {
+	.name = "CXP22",
+	.erd = 599,
+	.weight = 514,
+};
+
+struct rim_data al22 ={
+	.name = "AL22",
+	.erd = 595,
+	.weight = 385,
+};
